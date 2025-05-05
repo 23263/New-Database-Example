005dfa37942696261e7fe922b24e2e33eb60057e
23263 <23263@burnside.school.nz>
2025-05-05 12:33:46 +1200

    starter print results

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print('Hello, World')
+import sqlite3
+
+db = sqlite3.connect("footballer.db")
+cursor = db.cursor()
+sql = "SELECT * FROM footballer;"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
+
+
+db.close
