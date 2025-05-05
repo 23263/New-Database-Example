7e6b46f8b3d144955dbd74a78a7dce09aacb001f
23263 <23263@burnside.school.nz>
2025-05-05 19:43:12 +1200

    work 2

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+#docstring - Ben Jorgensen - footballer database application
+#imports
 import sqlite3
 
-db = sqlite3.connect("footballer.db")
-cursor = db.cursor()
-sql = "SELECT * FROM footballer;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
+#constants and variables
+DATABASE = "footballer.db"
 
+#functions
+def print_all_footballer():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM footballer;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(results)
+    db.close
 
-db.close
+#main code
+for footballer in results
+    print(f"Footballer name: ")
