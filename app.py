e6a518e663885ce5373cbd8de87845650599e46c
23263 <23263@burnside.school.nz>
2025-05-08 09:05:44 +1200

    making functions

--- app.py
+++ app.py
@@ -11,9 +11,53 @@ def print_all_footballer():
     cursor = db.cursor()
     sql = "SELECT * FROM footballer;"
     cursor.execute(sql)
-    results = cursor.fetchall()
-    print(f"Footballer: {footballer[1]} Goals: {footballer[2]} National team: {footballer[3]}")
+    footballer = cursor.fetchall()
+    print("Footballer:            Goals:     National team:")
+    for footballer in footballer:
+        print(f"{footballer[1] :<22} {footballer[2]:<10} {footballer[3]}")
     db.close
 
+def print_all_footballer_by_goals_asc():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM footballer ORDER BY Goals ASC;"
+    cursor.execute(sql)
+    footballer = cursor.fetchall()
+    print("Footballer:            Goals:     National team:")
+    for footballer in footballer:
+        print(f"{footballer[1] :<22} {footballer[2]:<10} {footballer[3]}")
+    db.close
+
+def print_all_footballer_by_goals_desc():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM footballer ORDER BY Goals DESC;"
+    cursor.execute(sql)
+    footballer = cursor.fetchall()
+    print("Footballer:            Goals:     National team:")
+    for footballer in footballer:
+        print(f"{footballer[1] :<22} {footballer[2]:<10} {footballer[3]}")
+    db.close
+
+def search_footballers_by_nationality():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM footballer ORDER BY Goals DESC;"
+    cursor.execute(sql)
+    footballer = cursor.fetchall()
+
+
+
+
+
+
+
+
+
+#cursor = db.cursor()
+#sql = "SELECT * FROM footballer;"
+#cursor.execute(sql)
+#footballer = cursor.fetchall()
+
 #main code
-print_all_footballer
+print_all_footballer_by_goals_desc()
