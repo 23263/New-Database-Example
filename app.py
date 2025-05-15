2a5e6be5ac09e183879be9707360abc1dad2b324
23263 <23263@burnside.school.nz>
2025-05-15 21:12:26 +1200

    Finished error testing

--- app.py
+++ app.py
@@ -59,29 +59,38 @@ def search_players_by_more_goals_than_response():
         print("That is not a number")
 
 def search_footballers_by_nationality():
+    #list of nationalities
+    nationalities = {"Argentina", "Netherlands", "Egypt", "England", "Norway", "Uruguay", "Brazil", "Spain", "France", "Italy", "Germany", "Switzerland", "Georgia"}
     #print all footballer data with searched nationality
     try:
         db = sqlite3.connect(DATABASE)
         cursor = db.cursor()
-        nationality_response = int(input("What nationality? "))
+        nationality_response = input("What nationality? ")
         nationality_response_title = nationality_response.title()
+        if nationality_response_title not in nationalities:
+            raise ValueError
         sql = "SELECT * FROM footballer WHERE nationality = ?;" 
         cursor.execute(sql, (nationality_response_title,))
         footballer = cursor.fetchall()
         print("Footballer:            Goals:     National team:")
         for footballer in footballer:
-         print(f"{footballer[1] :<22} {footballer[2]:<10} {footballer[3]}")
+            print(f"{footballer[1] :<22} {footballer[2]:<10} {footballer[3]}")
         db.close()
-    except ValueError:
+    except ValueError as Error:
         print("That is not a country")
+        
 
 def search_footballers_by_club_name():
+    #list of clubs
+    clubs = "Liverpool", "Arsenal", "Real Madrid", "Barcelona", "Inter Milan", "Bayern Munich", "Bayer Leverkusen", "Paris Saint Germain"
     #print all footballer data with searched club
     try:
         db = sqlite3.connect(DATABASE)
         cursor = db.cursor()
         play_club_response = input("What club do they play for? ")
         play_club_response_title = play_club_response.title()
+        if play_club_response_title not in clubs:
+            raise ValueError
         sql = "SELECT Footballer.name, Footballer.goals, Footballer.nationality FROM Footballer JOIN Teams ON Footballer.team_id = Teams.team_id WHERE Teams.team_name = ?;" 
         cursor.execute(sql, (play_club_response_title,))
         footballer = cursor.fetchall()
@@ -89,39 +98,47 @@ def search_footballers_by_club_name():
         for footballer in footballer:
             print(f"{footballer[0] :<22} {footballer[1]:<10} {footballer[2]}")
         db.close()
-    except ValueError:
+    except ValueError as Error:
         print("That is not a club name")
 
 def search_team_name_for_home_ground():
+    #list of team names
+    clubs = "Liverpool", "Arsenal", "Real Madrid", "Barcelona", "Inter Milan", "Bayern Munich", "Bayer Leverkusen", "Paris Saint Germain"
     #search for a club with the home ground name
     try:
         db = sqlite3.connect(DATABASE)
         cursor = db.cursor()
-        home_ground_club_response = input("What clubs home ground are you looking for? ")
-        home_ground_club_response_title = home_ground_club_response.title()
+        club_response = input("What clubs home ground are you looking for? ")
+        club_response_title = club_response.title()
+        if club_response_title not in clubs:
+            raise ValueError
         sql = "SELECT * FROM Teams WHERE team_name = ?;" 
-        cursor.execute(sql, (home_ground_club_response_title,))
+        cursor.execute(sql, (club_response_title,))
         home_ground = cursor.fetchall()
         print("Club team:            Home ground:")
         for footballer in home_ground:
             print(f"{footballer[1] :<21} {footballer[2]}")
-    except ValueError:
+    except ValueError as Errors:
         print("That is not a club name")
 
 def search_home_ground_for_team_name():
+    #list of home ground names
+    home_grounds = "Anfield", "Emirates Stadium", "Santiago Bernabeu Stadium", "Estadi Olimpic Lluis Companys", "San Siro", "Allianz Arena", "Bay Arena", "Parc Des Princes"
     #search for a home ground with the club name
     try:
         db = sqlite3.connect(DATABASE)
         cursor = db.cursor()
-        home_ground_club_response = input("What home ground does the club your looking for play at? ")
-        home_ground_club_response_title = home_ground_club_response.title()
+        home_ground_response = input("What home ground does the club your looking for play at? ")
+        home_ground_response_title = home_ground_response.title()
+        if home_ground_response_title not in home_grounds:
+            raise ValueError
         sql = "SELECT * FROM Teams WHERE home_ground = ?;" 
-        cursor.execute(sql, (home_ground_club_response_title,))
+        cursor.execute(sql, (home_ground_response_title,))
         club = cursor.fetchall()
         print("Home Ground:            Club:")
         for footballer in club:
             print(f"{footballer[2] :<23} {footballer[1]}")
-    except ValueError:
+    except ValueError as Errors:
         print("That is not a home ground")
 
 
