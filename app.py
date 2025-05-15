2006b522598cf9b81a8fcd74936844b7d8c9197f
23263 <23263@burnside.school.nz>
2025-05-15 12:44:52 +1200

    Error testing

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ DATABASE = "footballer.db"
 
 #functions
 def print_all_footballer():
+    #print all footballer data
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
     sql = "SELECT * FROM footballer;"
@@ -18,6 +19,7 @@ def print_all_footballer():
     db.close()
 
 def print_all_footballer_by_goals_asc():
+    #print all footballer data by goals from least to most
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
     sql = "SELECT * FROM footballer ORDER BY Goals ASC;"
@@ -29,6 +31,7 @@ def print_all_footballer_by_goals_asc():
     db.close()
 
 def print_all_footballer_by_goals_desc():
+    #print all footballers by goals from most to least
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
     sql = "SELECT * FROM footballer ORDER BY Goals DESC;"
@@ -40,67 +43,86 @@ def print_all_footballer_by_goals_desc():
     db.close()
 
 def search_players_by_more_goals_than_response():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    more_goals_than_response = input("How many goals should the player have more than? ")
-    sql = "SELECT * FROM Footballer WHERE goals > ? ORDER BY goals ASC"
-    cursor.execute(sql, (more_goals_than_response,))
-    goals = cursor.fetchall()
-    print("Footballer:            Goals:     National team:")
-    for footballer in goals:
-        print(f"{footballer[1] :<22} {footballer[2]:<10} {footballer[3]}")
-    db.close()
+    try:
+        #print all footballer footballer data with more goals than response
+        db = sqlite3.connect(DATABASE)
+        cursor = db.cursor()
+        more_goals_than_response = int(input("How many goals should the player have more than? "))
+        sql = "SELECT * FROM Footballer WHERE goals > ? ORDER BY goals ASC"
+        cursor.execute(sql, (more_goals_than_response,))
+        goals = cursor.fetchall()
+        print("Footballer:            Goals:     National team:")
+        for footballer in goals:
+            print(f"{footballer[1] :<22} {footballer[2]:<10} {footballer[3]}")
+        db.close()
+    except ValueError:
+        print("That is not a number")
 
 def search_footballers_by_nationality():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    nationality_response = input("What nationality? ")
-    nationality_response_title = nationality_response.title()
-    sql = "SELECT * FROM footballer WHERE nationality = ?;" 
-    cursor.execute(sql, (nationality_response_title,))
-    footballer = cursor.fetchall()
-    print("Footballer:            Goals:     National team:")
-    for footballer in footballer:
-     print(f"{footballer[1] :<22} {footballer[2]:<10} {footballer[3]}")
-    db.close()
+    #print all footballer data with searched nationality
+    try:
+        db = sqlite3.connect(DATABASE)
+        cursor = db.cursor()
+        nationality_response = int(input("What nationality? "))
+        nationality_response_title = nationality_response.title()
+        sql = "SELECT * FROM footballer WHERE nationality = ?;" 
+        cursor.execute(sql, (nationality_response_title,))
+        footballer = cursor.fetchall()
+        print("Footballer:            Goals:     National team:")
+        for footballer in footballer:
+         print(f"{footballer[1] :<22} {footballer[2]:<10} {footballer[3]}")
+        db.close()
+    except ValueError:
+        print("That is not a country")
 
 def search_footballers_by_club_name():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    play_club_response = input("What club do they play for? ")
-    play_club_response_title = play_club_response.title()
-    sql = "SELECT Footballer.name, Footballer.goals, Footballer.nationality FROM Footballer JOIN Teams ON Footballer.team_id = Teams.team_id WHERE Teams.team_name = ?;" 
-    cursor.execute(sql, (play_club_response_title,))
-    footballer = cursor.fetchall()
-    print("Footballer:            Goals:     National team:")
-    for footballer in footballer:
-     print(f"{footballer[0] :<22} {footballer[1]:<10} {footballer[2]}")
-    db.close()
+    #print all footballer data with searched club
+    try:
+        db = sqlite3.connect(DATABASE)
+        cursor = db.cursor()
+        play_club_response = input("What club do they play for? ")
+        play_club_response_title = play_club_response.title()
+        sql = "SELECT Footballer.name, Footballer.goals, Footballer.nationality FROM Footballer JOIN Teams ON Footballer.team_id = Teams.team_id WHERE Teams.team_name = ?;" 
+        cursor.execute(sql, (play_club_response_title,))
+        footballer = cursor.fetchall()
+        print("Footballer:            Goals:     National team:")
+        for footballer in footballer:
+            print(f"{footballer[0] :<22} {footballer[1]:<10} {footballer[2]}")
+        db.close()
+    except ValueError:
+        print("That is not a club name")
 
 def search_team_name_for_home_ground():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    home_ground_club_response = input("What clubs home ground are you looking for? ")
-    home_ground_club_response_title = home_ground_club_response.title()
-    sql = "SELECT * FROM Teams WHERE team_name = ?;" 
-    cursor.execute(sql, (home_ground_club_response_title,))
-    home_ground = cursor.fetchall()
-    print("Club team:            Home ground:")
-    for footballer in home_ground:
-        print(f"{footballer[1] :<21} {footballer[2]}")
+    #search for a club with the home ground name
+    try:
+        db = sqlite3.connect(DATABASE)
+        cursor = db.cursor()
+        home_ground_club_response = input("What clubs home ground are you looking for? ")
+        home_ground_club_response_title = home_ground_club_response.title()
+        sql = "SELECT * FROM Teams WHERE team_name = ?;" 
+        cursor.execute(sql, (home_ground_club_response_title,))
+        home_ground = cursor.fetchall()
+        print("Club team:            Home ground:")
+        for footballer in home_ground:
+            print(f"{footballer[1] :<21} {footballer[2]}")
+    except ValueError:
+        print("That is not a club name")
 
 def search_home_ground_for_team_name():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    home_ground_club_response = input("What home ground does the club your looking for play at? ")
-    home_ground_club_response_title = home_ground_club_response.title()
-    sql = "SELECT * FROM Teams WHERE home_ground = ?;" 
-    cursor.execute(sql, (home_ground_club_response_title,))
-    club = cursor.fetchall()
-    print("Home Ground:            Club:")
-    for footballer in club:
-        print(f"{footballer[2] :<23} {footballer[1]}")
-
+    #search for a home ground with the club name
+    try:
+        db = sqlite3.connect(DATABASE)
+        cursor = db.cursor()
+        home_ground_club_response = input("What home ground does the club your looking for play at? ")
+        home_ground_club_response_title = home_ground_club_response.title()
+        sql = "SELECT * FROM Teams WHERE home_ground = ?;" 
+        cursor.execute(sql, (home_ground_club_response_title,))
+        club = cursor.fetchall()
+        print("Home Ground:            Club:")
+        for footballer in club:
+            print(f"{footballer[2] :<23} {footballer[1]}")
+    except ValueError:
+        print("That is not a home ground")
 
 
 #main code
@@ -108,12 +130,13 @@ while True:
     user_input = input("What would you like to do?\n"
                         "1: Print all footballer data\n"
                         "2: Print all footballers by goals from least to most\n" 
-                        "3: print all footballers by goals from most to least\n"
-                        "4: Search footballers by nationality\n"
-                        "5: Search footballer by club name\n"
-                        "6: Search for team name by home ground\n"
-                        "7: Search for home ground by team name\n"
-                        "8: Stop\n")
+                        "3: Print all footballers by goals from most to least\n"
+                        "4: Print all footballers with more goals than response\n"
+                        "5: Search footballers by nationality\n"
+                        "6: Search footballer by club name\n"
+                        "7: Search for team name by home ground\n"
+                        "8: Search for home ground by team name\n"
+                        "9: Stop\n")
     if user_input ==  "1":
         print_all_footballer()
     elif user_input == "2":
